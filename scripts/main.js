f656aaf5aba2e8b7232ed9ed8b32e35c7cdfbd3b
mohammed jamal <mohammadjamalofficals@gmail.com>
2025-01-26 09:46:04 +0000

    fixed issues with the menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,19 @@
 const headerBarIcon = document.querySelector(".header__bars");
 const moblieNavEl = document.querySelector(".moblie--nav");
+const linksArr = document.querySelectorAll(".moblie--nav__menu");
+console.log(linksArr);
 if (headerBarIcon && moblieNavEl) {
   headerBarIcon.addEventListener("click", () => {
     moblieNavEl.classList.toggle("active");
+    document.body.style.overflowY = "hidden";
   });
 }
+linksArr.forEach((link) => {
+  link.addEventListener("click", () => {
+    moblieNavEl.classList.remove("active");
+    document.body.style.overflowY = "auto";
+  });
+});
 // Handle resizing to ensure correct menu state
 window.addEventListener("resize", () => {
   if (window.innerWidth > 768) {
